e16294b769021126fe05b867560c314dc8bd1c6e
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2019-12-19 11:56:55 +0530

    Take grid count from user

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+		
+		int grid;
+		
+		System.out.println("Enter grid number: ");
+		grid = scanner.nextInt();
+	}
+}
